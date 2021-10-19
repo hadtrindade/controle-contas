59dad9faac2ebccc941f0f142630b2dfa8af343b
Haddly Trindade <haddleytrindade@gmail.com>
2021-10-18 22:20:36 -0300

    correção do carregamento do banco

--- controle_contas/ext/db/__init__.py
+++ controle_contas/ext/db/__init__.py
@@ -1,19 +1,18 @@
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from decouple import config
+from os import getenv
 
 db = SQLAlchemy()
 migrade = Migrate()
 
-if config("FLASK_DEBUG", default=False):
-    DATABASE_URI = config("DATABASE_URL_SQLITE", default="sqlite:///controle_contas.db")
-else:
-    DATABASE_URI = config("DATABASE_URL").replace("://", "ql://", 1)
-
 
 def init_app(app):
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    if not app.config["DEBUG"]:
+
+        app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace(
+            "://", "ql://", 1
+        )
     db.init_app(app)
     app.db = db
     migrade.init_app(app, db)
