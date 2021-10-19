1f8f4654abd6f08f0c79980e8030a80e6b081998
Haddly Trindade <haddleytrindade@gmail.com>
2021-10-18 22:31:58 -0300

    update file

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -12,4 +12,4 @@ def test_deve_falhar_ao_gerar_um_token(client):
     user = {"username": "test", "password": "12323"}
     esperado = "Wrong username or password"
     response = client.post(url_for("auth.get_token"), json=user)
-    assert response.json['message'] == esperado
+    assert response.json["message"] == esperado
