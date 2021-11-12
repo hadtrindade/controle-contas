f89c7805e3bb83d8532e1961cf4bccea19495369
Haddly Trindade <haddleytrindade@gmail.com>
2021-11-11 22:51:31 -0300

    Corrigido o acesso ao admin do db

--- controle_contas/ext/login.py
+++ controle_contas/ext/login.py
@@ -1,4 +1,4 @@
-from flask_login import LoginManager
+from flask_login import LoginManager, AnonymousUserMixin
 from controle_contas.ext.auth.models import User
 from flask import redirect, url_for
 
@@ -6,9 +6,17 @@ from flask import redirect, url_for
 login_manager = LoginManager()
 
 
+class CCAnonymousUser(AnonymousUserMixin):
+
+    @property
+    def is_staff(self):
+        return False
+
+
 def init_app(app):
 
     login_manager.init_app(app)
+    login_manager.anonymous_user = CCAnonymousUser
 
     @login_manager.user_loader
     def load_user(user_id):
